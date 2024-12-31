78a4197478df45635ecc2b4800eb93cd2550f98c
alerod1999 <alexandregonzalezcpge@gmail.com>
2024-12-31 16:51:20 +0100

    A transition has been added with vue js.

--- src/Home.vue
+++ src/Home.vue
@@ -1,8 +1,30 @@
 <template>
 
-   
-    
-    </template>
-    <script setup>
-    
-    </script>
+<button @click="toggleSpoiler">Display / Hide spoiler </button>
+<Transition>
+    <div v-if="showSpoiler" class="spoiler">
+At the end of the episode, the main character dies !
+
+</div>
+</Transition>
+
+
+</template>
+<script setup>
+import { ref } from 'vue';
+
+const showSpoiler = ref(false)
+const toggleSpoiler = () => showSpoiler.value = !showSpoiler.value
+</script>
+<style>
+.spoiler {
+    padding: 5rem;
+    border: 1px solid white;
+    transition: .5s;
+}
+.v-enter-from,
+.v-leave-to {
+    opacity: 0;
+}
+
+</style>
